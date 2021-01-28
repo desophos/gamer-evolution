6bc6e82d849d157a84f3085799e4334cdba8c5f6
desophos <1887450+desophos@users.noreply.github.com>
2021-01-28 15:19:56 -0600

    Run game for a population & collect results

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.List
 import Test.QuickCheck
 import Lib
 
@@ -11,7 +12,16 @@ dilemma [1,0] = [5,0]
 dilemma [0,1] = [0,5]
 dilemma [1,1] = [1,1]
 
+sameMatch :: (Eq a) => [a] -> [a] -> Bool
+sameMatch xs ys = all (`elem` ys) xs
+
+matchups2 :: (Eq a) => [a] -> [[a]]
+matchups2 xs = nubBy sameMatch [[x,y] | x <- xs, y <- xs, x /= y]
+
 main :: IO ()
 main =
-    let players = vectorOf 2 randomChromosome
-    in print =<< generate (playGame dilemma 3 <$> players)
+    let players = vectorOf 20 randomChromosome
+        matches = fmap matchups2 players
+        runMatch = playGame dilemma 3
+        results = map runMatch <$> matches
+    in print =<< generate results
