0e9447312c933f8e1c98cbc07b58911bcb5e6290
desophos <1887450+desophos@users.noreply.github.com>
2021-02-02 23:55:12 -0600

    Clean up Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
-import Data.List
 import Test.QuickCheck
 import Lib
+import Evolution
+import Util
+
 
 -- reward matrix for Prisoner's Dilemma
 -- 0 = cooperate; 1 = defect
@@ -12,16 +14,11 @@ dilemma [1,0] = [5,0]
 dilemma [0,1] = [0,5]
 dilemma [1,1] = [1,1]
 
-sameMatch :: (Eq a) => [a] -> [a] -> Bool
-sameMatch xs ys = all (`elem` ys) xs
-
-matchups2 :: (Eq a) => [a] -> [[a]]
-matchups2 xs = nubBy sameMatch [[x,y] | x <- xs, y <- xs, x /= y]
 
 main :: IO ()
 main =
-    let players = vectorOf 20 randomChromosome
-        matches = fmap matchups2 players
+    let players = map agentChromosome <$> newPlayers 20
         runMatch = playGame dilemma 3
+        matches = fmap matchups2 players
         results = map runMatch <$> matches
     in print =<< generate results
