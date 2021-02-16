b4ba5229ce445004ecca5165baf9053a043f2dde
desophos <1887450+desophos@users.noreply.github.com>
2021-02-16 01:40:27 -0600

    include TestEvolution in test runner

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,12 @@
 module Main where
 
 import qualified TestUtil as U
+import qualified TestEvolution as E
 import System.Exit
 
 main :: IO ()
 main = do
-    good <- and <$> sequence [U.runTests]
+    good <- and <$> sequence [U.runTests, E.runTests]
     if good
         then exitSuccess
         else exitFailure
