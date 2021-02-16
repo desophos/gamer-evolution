f23adfd7c6766a733918522b89049b26319ca706
desophos <1887450+desophos@users.noreply.github.com>
2021-02-16 01:31:02 -0600

    fix `Show (Agent a)` instance

--- test/Instances.hs
+++ test/Instances.hs
@@ -1,12 +1,12 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, StandaloneDeriving #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Instances where
 
 import Data.Typeable
-import Evolution (Agent)
+import Evolution (Agent(..))
 
 instance (Typeable a, Typeable b) => Show (a -> b) where
     showsPrec _ _ = showParen True $ showString "_ :: " . (showsTypeRep . typeRep) p
         where p = Proxy :: Proxy (a -> b)
 
-instance Show a => Show (Agent a)
+deriving instance (Show a, Typeable a) => Show (Agent a)
