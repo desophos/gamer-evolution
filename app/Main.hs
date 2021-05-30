220dc4635b5c4fee3be7bca0ee83f816e3e250cd
desophos <1887450+desophos@users.noreply.github.com>
2021-05-30 00:27:34 -0500

    Main is now an example of library usage
    It graphs the fitness of an example population over time.

--- app/Main.hs
+++ app/Main.hs
@@ -2,21 +2,52 @@
 module Main where
 
 import qualified Data.Set                      as S
-import           Evolution
-import           Gamer
-import           Test.QuickCheck
-import           Util
+import           Evolution                      ( Agent
+                                                , EvolutionParams(..)
+                                                , avgFitness
+                                                , collectEvolve
+                                                )
+import           GHC.Float                      ( float2Int
+                                                , int2Float
+                                                )
+import           Gamer                          ( GamerParams(..)
+                                                , dilemma
+                                                , genPlayers
+                                                , playGame
+                                                )
+import           Graphics.EasyPlot              ( Graph2D(Function2D)
+                                                , Option(Title)
+                                                , Option2D(Range, Step)
+                                                , Plot(plot)
+                                                , TerminalType(PNG)
+                                                )
+import           Test.QuickCheck                ( generate )
+import           Util                           ( matchups )
 
 
-main :: IO ()
-main =
-    let params = GamerParams { .. }
-              where
-                gamerActions = 2
-                gamerStates  = 8
-                gamerMemory  = 1
-        players  = map agentGenome <$> genPlayers params 20
-        runMatch = playGame dilemma 3
-        matches  = fmap (matchups 2 . S.fromDistinctAscList) players
-        results  = map runMatch <$> matches
-    in  print =<< generate results
+main :: IO Bool
+main = graphFitness
+    =<< generate (collectEvolve eParams game matchup =<< players)
+  where
+    gParams = GamerParams { .. }
+      where
+        gamerActions = 2
+        gamerStates  = 8
+        gamerMemory  = 3
+    eParams = EvolutionParams { .. }
+      where
+        evolveGenerations = 50
+        evolvePopSize     = 50
+        evolveSurvivors   = 30
+        evolveMutateP     = 0.01
+    game    = playGame dilemma 50
+    players = genPlayers gParams 50
+    matchup = matchups 2 . S.fromDistinctAscList
+
+
+graphFitness :: [[Agent a]] -> IO Bool
+graphFitness run = plot (PNG "plot.png") $ Function2D
+    [Title "Average Fitness"]
+    [Range 0 (int2Float $ length avgRun - 1), Step 1]
+    ((avgRun !!) . float2Int)
+    where avgRun = map avgFitness run
